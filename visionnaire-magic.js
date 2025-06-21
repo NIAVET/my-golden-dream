028c4a73966055386347618a10da6c21c808b727
NIAVET <135268210+NIAVET@users.noreply.github.com>
2025-06-21 11:26:08 +0200

    Update visionnaire-magic.js
    function showVision() {
        alert("✨ Numéro fétiche du jour : 12\n💬 'Jou ka vini pli bèl si ou kwè an li.'");
    }

--- visionnaire-magic.js
+++ visionnaire-magic.js
@@ -1,3 +1,6 @@
+function showVision() {
+    alert("✨ Numéro fétiche du jour : 12\n💬 'Jou ka vini pli bèl si ou kwè an li.'");
+}
 // Module Visionnaire Magic
 function afficherVision() {
   alert("Rèv-la sé on senfoni ka fè nou sonjé ki pli bèl-la ka vini.");
